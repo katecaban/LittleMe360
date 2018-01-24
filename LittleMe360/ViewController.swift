2b0cf58ae8849ce4ea31ddb92cb7c9d83810331e
Kate Caban <katecaban@Kates-Mac-mini.local>
2018-01-23 20:18:29 -0500

    Init

--- LittleMe360/ViewController.swift
+++ LittleMe360/ViewController.swift
@@ -7,12 +7,51 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // start up the camera
+        
+        let captureSession = AVCaptureSession()
+        
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        view.layer.addSublayer(previewLayer)
+        
+        previewLayer.frame = view.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        
+        captureSession.addOutput(dataOutput)
+        
+        // let request = VNCoreMLRequest(model: <#T##VNCoreMLModel#>, completionHandler: <#T##VNRequestCompletionHandler?##VNRequestCompletionHandler?##(VNRequest, Error?) -> Void#>)
+        
+        // VNImageRequestHandler(cgImage: <#T##CGImage#>, options: [:]).perform(<#T##requests: [VNRequest]##[VNRequest]#>)
+        
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        // print("Camera captured a frame:", Date())
+        
     }
 
     override func didReceiveMemoryWarning() {
